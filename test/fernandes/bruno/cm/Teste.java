e015e04afc2a9fbb904f4ad05d1d08f91aff541e
brunoFernandes <bwendel26999@gmail.com>
2020-10-19 07:25:09 -0300

    Inicializando projeto com JUnit5 configurado

---
+++ test/fernandes/bruno/cm/Teste.java
@@ -0,0 +1,21 @@
+package fernandes.bruno.cm;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Teste {
+
+//	@Test
+//	void test() {
+//		fail("Not yet implemented");
+//	}
+	
+	@Test
+	void testarSeIgualADois() {
+		int a = 1 + 1;
+		
+		assertEquals(2, a);
+	}
+
+}
