4ad3b2a3aa886d62ea927445f016aaf4f83b5f9f
brunoFernandes <bwendel26999@gmail.com>
2020-11-04 02:30:23 -0300

    Implementando funcao que adicionaVizinho e o teste que verifica o campo...

---
+++ test/fernandes/bruno/cm/modelo/CampoTeste.java
@@ -0,0 +1,26 @@
+package test.fernandes.bruno.cm.modelo;
+
+import fernandes.bruno.cm.modelo.Campo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CampoTeste {
+
+    private Campo campo;
+
+    @BeforeEach
+    void iniciarCampo() {
+        campo = new Campo(3, 3);
+    }
+
+    @Test
+    void testeVizinhoRealDistancia1() {
+        Campo vizinho = new Campo(3, 2);
+
+        boolean resultado = campo.adicionarVizinho(vizinho);
+
+        assertTrue(resultado);
+    }
+}
